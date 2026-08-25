6e4be2ec8d8353abd8f017eb33a13048f7823421
1453manamana5-web <1453manamana5@gmail.com>
2026-08-25 16:46:34 +0900

    Connect admin data to control dashboard

--- control-app/src/main.tsx
+++ control-app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import DeviceAccessGate from "./DeviceAccessGate";
 import PredictiveOpsOverlay from "./PredictiveOpsOverlay";
+import AdminOpsBridge from "./AdminOpsBridge";
 import CopilotLanguageExpansionBridge from "./CopilotLanguageExpansionBridge";
 import CopilotRemoteCommandBridge from "./CopilotRemoteCommandBridge";
 import CopilotCapabilityBridge from "./CopilotCapabilityBridge";
@@ -14,6 +15,7 @@ import "./experimental-nav.css";
 import "./predictive-ops.css";
 import "./predictive-correlation.css";
 import "./copilot-capabilities.css";
+import "./admin-ops.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -21,6 +23,7 @@ createRoot(document.getElementById("root")!).render(
       <>
         <App database={db} />
         <PredictiveOpsOverlay database={db} />
+        <AdminOpsBridge database={db} />
         <CopilotLanguageExpansionBridge />
         <CopilotRemoteCommandBridge database={db} />
         <CopilotCapabilityBridge database={db} />
