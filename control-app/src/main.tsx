f87244aee1cae6bfc306343d94a4d23412411e62
1453manamana5-web <1453manamana5@gmail.com>
2026-08-25 14:26:08 +0900

    Mount predictive operations overlay

--- control-app/src/main.tsx
+++ control-app/src/main.tsx
@@ -2,14 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import DeviceAccessGate from "./DeviceAccessGate";
+import PredictiveOpsOverlay from "./PredictiveOpsOverlay";
 import { db } from "./firebase";
 import "./index.css";
 import "./experimental-nav.css";
+import "./predictive-ops.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <DeviceAccessGate>
-      <App database={db} />
+      <>
+        <App database={db} />
+        <PredictiveOpsOverlay database={db} />
+      </>
     </DeviceAccessGate>
   </StrictMode>
 );
