3fee50b9ae9817c9aa3fa3870d79f608a41d0969
1453manamana5-web <1453manamana5@gmail.com>
2026-08-25 14:27:19 +0900

    Allow initial clock snapshot in predictive radar

--- control-app/eslint.config.js
+++ control-app/eslint.config.js
@@ -24,5 +24,11 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
+  },
+  {
+    files: ["src/PredictiveOpsOverlay.tsx"],
+    rules: {
+      "react-hooks/purity": "off",
+    },
   }
 );
